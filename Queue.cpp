abd4a276e04caee9e7e0157cd6c4f703c21c2846
ayonatan <102904006+ayonatan@users.noreply.github.com>
2022-05-29 22:52:06 +0300

    עובר טסטים על תור של אינטים

--- Queue.cpp
+++ Queue.cpp
@@ -10,44 +10,35 @@ Queue::Queue() {
 
 //template <class T>
 //Queue<T>::Queue(const Queue& queue){
-Queue::Queue(const Queue &queue) 
-{
+Queue::Queue(const Queue &queue) {
     m_data = new int[queue.m_maxSize];
     m_maxSize = queue.m_maxSize;
     m_lastItemIndex = queue.m_lastItemIndex;
-    try 
-    {
-        for (int i = 0; i < m_maxSize; i++) 
-        {
+    try {
+        for (int i = 0; i < m_maxSize; i++) {
             m_data[i] = queue.m_data[i];
             //need check with michal
         }
     }
-    catch (...) 
-    {
+    catch (...) {
         delete[] m_data;
         throw;
     }
 }
 
 //template <class T>
-Queue& Queue::operator=(const Queue &queue)
-{
-    if (this == &queue) 
-    {
+Queue &Queue::operator=(const Queue &queue) {
+    if (this == &queue) {
         return *this;
     }
     T *tempData = new T[queue.m_maxSize];
-    try 
-    {
+    try {
         m_maxSize = queue.m_maxSize;
         m_lastItemIndex = queue.m_lastItemIndex;
-        for (int i = 0; i < m_lastItemIndex; ++i) 
-        {
+        for (int i = 0; i < m_lastItemIndex; ++i) {
             m_data[i] = queue.m_data[i];
         }
-    } catch (...) 
-    {
+    } catch (...) {
         delete[] tempData;
         throw;
     }
@@ -56,19 +47,16 @@ Queue& Queue::operator=(const Queue &queue)
     return *this;
 }
 
-Queue::~Queue() 
-{
+Queue::~Queue() {
     delete[] m_data;
 }
 
 
 ///////
 //template <class T>
-void Queue::pushBack(const T item) 
-{
+void Queue::pushBack(const T item) {
     // if item == null ?
-    if (m_lastItemIndex >= m_maxSize) 
-    {
+    if (m_lastItemIndex + 1 >= m_maxSize) {
         expand();
     }
     m_data[m_lastItemIndex] = item;
@@ -76,19 +64,15 @@ void Queue::pushBack(const T item)
 }
 
 //<template class T>
-void Queue::expand() 
-{
+void Queue::expand() {
     int newSize = m_maxSize * EXPAND_RATE;
     T *tempData = new T[newSize];
-    try 
-    {
-        for (int i = 0; i < m_maxSize; ++i) 
-        {
+    try {
+        for (int i = 0; i < m_maxSize; ++i) {
             tempData[i] = m_data[i];
         }
     }
-    catch (...) 
-    {
+    catch (...) {
         delete[] tempData;
         throw;
     }
@@ -98,53 +82,58 @@ void Queue::expand()
 }
 
 //<template class T>
-int Queue::size() const 
-{
+int Queue::size() const {
+    //return m_lastItemIndex;
     return m_lastItemIndex;
 }
 
 //<template class T>
-T Queue::front() const 
-{
-    return m_data[m_lastItemIndex - 1];
+T Queue::front() const {
+    return m_data[0];
 }
 
 
 //<template class T>
-void Queue::popFront() 
-{
-    if (m_lastItemIndex == 0) 
-    {
+void Queue::popFront() {
+    if (m_lastItemIndex == 0) {
         throw EmptyQueue();
     }
     moveOneLeft();
-    if (m_lastItemIndex - 1 <= 0.5 * m_maxSize) 
-    {
+    if (m_lastItemIndex - 1 <= 0.5 * m_maxSize) {
         minimize();
     }
 }
 
-void Queue::moveOneLeft() 
-{
-    for (int i = 0; i < m_lastItemIndex - 1; ++i) 
-    {
+void Queue::moveOneLeft() {
+    for (int i = 0; i < m_lastItemIndex - 1; ++i) {
         m_data[i] = m_data[i + 1];
     }
     m_lastItemIndex--;
 }
 
-void Queue::minimize() 
-{
-    delete[] (m_data+(int)(m_maxSize * MINIMIZE_RATE));
-    m_maxSize = m_maxSize * MINIMIZE_RATE;
-} 
+void Queue::minimize() {
+    int newSize = m_maxSize * MINIMIZE_RATE;
+    T *tempData = new T[newSize];
+    try {
+        for (int i = 0; i < m_lastItemIndex; ++i) {
+            tempData[i] = m_data[i];
+        }
+    }
+    catch (...) {
+        delete[] tempData;
+        throw;
+    }
+    delete[] m_data;
+    m_data = tempData;
+    m_maxSize = newSize;
+}
 
 Queue Queue::filter(Queue &queue, FilterFunc filterFunc) {
     // what if given queue smaller
     Queue newQueue;
-    for (int i = 0; i < queue.m_lastItemIndex; ++i) {
-        if (filterFunc(queue.m_data[i])) {
-            newQueue.pushBack(queue.m_data[i]);
+    for (Queue::Iterator it = queue.begin(); it != queue.end(); ++it) {
+        if (filterFunc(*it)) {
+            newQueue.pushBack(*it);
         }
     }
     return newQueue;
@@ -152,89 +141,73 @@ Queue Queue::filter(Queue &queue, FilterFunc filterFunc) {
 
 void Queue::transform(Queue &queue, Transform transformOperator) {
     // what if given queue smaller
-    for (int i = 0; i < queue.m_lastItemIndex; ++i) {
-        queue.m_data[i] = transformOperator(queue.m_data[i]);
+    for (Queue::Iterator it = queue.begin(); it != queue.end(); ++it) {
+        transformOperator(*it);
     }
 }
 
-Queue::Const_Iterator Queue::const_begin() const
-{
+Queue::Const_Iterator Queue::const_begin() const {
     return Const_Iterator(this, 0);
 }
 
-Queue::Const_Iterator Queue::const_end() const
-{
+Queue::Const_Iterator Queue::const_end() const {
     return Const_Iterator(this, m_lastItemIndex);
 }
 
-Queue::Iterator Queue::begin()
-{
+Queue::Iterator Queue::begin() {
     return Iterator(this, 0);
 }
 
-Queue::Iterator Queue::end()
-{
+Queue::Iterator Queue::end() {
     return Iterator(this, m_lastItemIndex);
 }
 
-Queue::Const_Iterator::Const_Iterator(const Queue* queue, int index) : 
-    m_queue(queue), m_index(index){}
+Queue::Const_Iterator::Const_Iterator(const Queue *queue, int index) :
+        m_queue(queue), m_index(index) {}
 
-const T& Queue::Const_Iterator::operator*()
-{
-    if (m_index>=m_queue->m_lastItemIndex)
-    {
+const T &Queue::Const_Iterator::operator*() {
+    if (m_index >= m_queue->m_lastItemIndex) {
         throw InvalidOperation();
     }
     return m_queue->m_data[m_index];
 }
 
-Queue::Const_Iterator& Queue::Const_Iterator::operator++()
-{
+Queue::Const_Iterator &Queue::Const_Iterator::operator++() {
     ++m_index;
-    if (m_index>=m_queue->m_lastItemIndex)
-    {
+    if (m_index >= m_queue->m_lastItemIndex) {
         throw InvalidOperation();
     }
     return *this;
 }
 
-bool Queue::Const_Iterator::operator!=(const Const_Iterator it) const
-{
-    if(m_queue!=it.m_queue)
-    {
+bool Queue::Const_Iterator::operator!=(const Const_Iterator it) const {
+    if (m_queue != it.m_queue) {
         throw InvalidOperation();
     }
-    return m_index!=it.m_index;
+    return m_index != it.m_index;
 }
 
-Queue::Iterator::Iterator(Queue* queue, int index) : 
-    m_queue(queue), m_index(index){}
+Queue::Iterator::Iterator(Queue *queue, int index) :
+        m_queue(queue), m_index(index) {}
 
-T& Queue::Iterator::operator*()
-{
-    if (m_index>=m_queue->m_lastItemIndex)
-    {
+T &Queue::Iterator::operator*() {
+    if (m_index >= m_queue->m_lastItemIndex) {
         throw InvalidOperation();
     }
     return m_queue->m_data[m_index];
 }
 
-Queue::Iterator& Queue::Iterator::operator++()
-{
+Queue::Iterator &Queue::Iterator::operator++() {
     ++m_index;
-    if (m_index>=m_queue->m_lastItemIndex)
-    {
+    if (m_index >= m_queue->m_lastItemIndex) {
         throw InvalidOperation();
     }
     return *this;
 }
 
-bool Queue::Iterator::operator!=(const Iterator it) const
-{
-    if(m_queue!=it.m_queue)
-    {
+bool Queue::Iterator::operator!=(const Iterator it) const {
+    if (m_queue != it.m_queue) {
         throw InvalidOperation();
     }
-    return m_index!=it.m_index;
+    return m_index != it.m_index;
 }
